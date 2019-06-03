f7348ca4485afe2f3115cb0c61f1160f6bfaac53
Max Kaminkehrer <max.kehl@posteo.de>
2019-06-03 09:42:30 +0200

    so

--- create_climate_diagrams.py
+++ create_climate_diagrams.py
@@ -1,23 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 08:44:48 2019
+
+@author: io
+"""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 # Import both data tables into python using pandas. Set the index column to "MESS_DATUM" and parse the column values as dates. [1P]
-garmisch  = 
-zugspitze = 
-
+garmisch  = pd.read_csv("/home/io/Dokumente/Python Kurs/exercise-4-Maxkehl/data/produkt_klima_tag_20171010_20190412_01550.txt", sep = ";", index_col="MESS_DATUM", parse_dates=["MESS_DATUM"])
+zugspitze = pd.read_csv("/home/io/Dokumente/Python Kurs/exercise-4-Maxkehl/data/produkt_klima_tag_20171010_20190412_05792.txt", sep = ";", index_col="MESS_DATUM", parse_dates=["MESS_DATUM"])
+garmisch.head()
+zugspitze.head()
+print(garmisch)
 # Clip the tables to the year 2018: [1P]
-garmisch  = 
-zugspitze = 
+garmisch  = garmisch.loc["2018"]
+zugspitze = zugspitze.loc ["2018"]
+print(garmisch)
 
 # Resample the temperature data to monthly averages (" TMK") and store them in simple lists: [1P]
-garmisch_agg  = 
-zugspitze_agg = 
-
+garmisch_agg  = list(garmisch.loc[:," TMK"].resample("1M").mean())
+zugspitze_agg = list(zugspitze.loc[:," TMK"].resample("1M").mean())
+print (garmisch_agg)
 # Define a plotting function that draws a simple climate diagram
 # Add the arguments as mentioned in the docstring below [1P]
 # Set the default temperature range from -15°C to 20°C and the precipitation range from 0mm to 370mm [1P]
-def create_climate_diagram(...):
+def create_climate_diagram(df,
+                           temp_col,
+                           prec_col,
+                           title,
+                           filename,
+                           temp_min=-15,
+                           temp_max=20,
+                           prec_min=0,
+                           prec_max=370):
     """
     Draw a climate diagram.
     
@@ -47,34 +66,35 @@ def create_climate_diagram(...):
     The figure
     
     """
-
     fig = plt.figure(figsize=(10,8))
     plt.rcParams['font.size'] = 16
 
     ax2 = fig.add_subplot(111)
-    ax1 = ax2.twinx()
+    #ax1 = ax2.twinx()
 
     # Draw temperature values as a red line and precipitation values as blue bars: [1P]
     # Hint: Check out the matplotlib documentation how to plot barcharts. Try to directly set the correct
     #       x-axis labels (month shortnames).
-    ax2.bar(...)
-    ax1.plot(...)
+    ax2.bar(df.index,height=df[prec_col],color="blue",width=20,label="precipitation")
+    #ax1.plot(df[temp_col],color="red",label"temperature") # invalid syntax
     
     # Set appropiate limits to each y-axis using the function arguments: [1P]
-    ax2.
-    ax1.
+    #ax2.
+    #ax1.
     
     # Set appropiate labels to each y-axis: [1P]
-    ax2.
-    ax1.
+    #ax2.
+    #ax1.
 
     # Give your diagram the title from the passed arguments: [1P]
-    plt.title(...)
+    plt.title(title)
 
     # Save the figure as png image in the "output" folder with the given filename. [1P]
+    plt.savefig(filename)
     
     return fig
 
 # Use this function to draw a climate diagram for 2018 for both stations and save the result: [1P]
-create_climate_diagram(...)
-create_climate_diagram(...)
+create_climate_diagram(garmisch, temp_col=" TMK", prec_col=" RSK", title="Climate Diagramm Garmisch 2018", filename= "/home/io/Dokumente/Python Kurs/exercise-4-Maxkehl/Climate_Garmisch_2018.png")
+
+create_climate_diagram(zugspitze, temp_col=" TMK", prec_col=" RSK", title="Climate Diagramm Zugspitze 2018", filename= "/home/io/Dokumente/Python Kurs/exercise-4-Maxkehl/Climate_Zugspitze_2018.png")
